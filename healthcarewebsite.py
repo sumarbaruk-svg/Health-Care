095dc27c967b5bf932530e392dd31e25242cfdca
sumarbaruk-svg <sumarbaruk@gmail.com>
2026-01-29 16:37:40 +0530

    Update healthcarewebsite.py

--- healthcarewebsite.py
+++ healthcarewebsite.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 # Unga Google verification code-a inga paste pannunga
 st.markdown("""
-    <meta name="google-site-verification" content="<meta name="google-site-verification" content="JuHPHIQRi7CC39Arg_RRNmDGZCAYjcpxH5A4jZDjgFk" />" />
+    <meta name="google-site-verification" content=<meta name="google-site-verification" content="JuHPHIQRi7CC39Arg_RRNmDGZCAYjcpxH5A4jZDjgFk" /> />
     """, unsafe_allow_html=True)
 
 st.markdown("""
